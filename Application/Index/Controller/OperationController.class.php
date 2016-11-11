ce17d88c71e33908e2765e2ec87aaf91b3843bd7
[liuyong] <[1026783720@qq.com]>
2016-11-11 17:06:34 +0800

    1111

--- Application/Index/Controller/OperationController.class.php
+++ Application/Index/Controller/OperationController.class.php
@@ -3,8 +3,34 @@ namespace Index\Controller;
 use Think\Controller;
 class OperationController extends Controller {
     public function index(){
-		$list=M('items')->where('pid=9')->select();
-		foreach
+		$pid = 1; //知识库总栏目对应的id
+		$where['pid'] = $pid;
+		if($_POST && !empty($_POST['keyword'])){
+			$keyword = I('post.keyword');
+		}
+		$list=M('items')->where($where)->select();
+		foreach($list as $k=>$v){
+			$list[$k]['news'] = M('news')->field('id,item_id,title')->where('item_id='.$v['id'])->select();
+		}
+		$this->assign('list',$list);
         $this->display();   
+	}
+	
+	public function getContent(){
+		$id = I('post.id');
+		$data = array();
+		if($id){
+			$list = M('news')->field('id,title,content,item_id')->where('id='.$id)->find();
+            $pname = M('items')->where('id='.$list['item_id'])->find();			
+            $list['pname'] = $pname['name'];
+			$data['status'] =1;
+            $data['msg'] = $list;			
+		}else{
+			$data['status'] = 0;
+			$datqa['msg'] = "对不起，你查询的信息不存在";
+		}
+		
+		$this->ajaxReturn($data);
+		
 	}
 }
--- Application/Runtime/Cache/Index/67441e23470169e874fc327e0413a7e8.php
+++ Application/Runtime/Cache/Index/67441e23470169e874fc327e0413a7e8.php
@@ -2,65 +2,55 @@
 <head>
 <title></title>
 <script src="/Public/js/jquery.min.js"></script> 
+<script src="/Public/js/bootstrap.min.js"></script>
+<link href="/Public/bootstrap/css/bootstrap.min.css"  rel="stylesheet" type="text/css" />
 
 <style>
-body{background:#f5f5f5;}
-#main{width:1108px;margin:0 auto;background:#fff;border:1px solid black;overflow:auto;}
-.head{height:300px;border:1px solid red;}
-.left{float:left;width:300px;height:1000px;border:1px solid red;}
-.right{float:right;width:790px;height:1000px;border:1px solid green;}
+body{background:#f5f5f5;margin:0px;padding:0px;}
+#main{width:1108px;margin:0 auto;overflow:auto;}
+.head{height:200px;background:#fff;}
+.htit{text-align:center;font-size:28px;padding:20px 0px 10px 0px;background:#afe5f7;}
+.search{margin-top:40px;text-align:center;}
+.searchbtn{height:40px;width:100px;}
+.keyword{width:300px;height:40px;}
+.left{float:left;width:300px;background:#fff;padding-bottom:40px;}
+.right{float:right;width:790px;background:#fff;padding-bottom:40px;}
 .stit{padding:10px 0px;text-align:center;background:#EC6C00;color:#fff;border-radius:10px 10px 0 0;font-size:18px;}
-.ptit{font-size:16px;color:#666;padding:10px 0px;border-bottom:1px solid #EAEAEA;}
-.icon{display:inline-block;height:14px;width:14px;background: #EC6C00;border-radius:7px;color:#fff;text-align:center;font-size:14px;font-weight:bold;margin:0px 10px;}
+.ptit{font-size:16px;color:#666;padding:10px 0px;border-bottom:1px solid #EAEAEA;cursor:pointer;font-weight:bold;}
+.icon{display:inline-block;height:14px;width:14px;line-height:14px;background: #EC6C00;border-radius:7px;color:#fff;text-align:center;font-size:14px;font-weight:normal;margin:0px 10px;}
 .otit{display:none;}
 .ctit{padding:10px 0px 10px 20px;font-size:14px;border-bottom: 1px solid #fff;background:#EAEAEA;}
-.ctit:hover{background:#fff;border-bottom: 1px solid #EAEAEA;}
+.ctit:hover{background:#fff;border-bottom: 1px solid #EAEAEA;cursor:pointer}
 .dtit{padding:10px 0px 10px 20px;background:#b9b8b8;color:#fff;border-radius:10px 10px 0 0;font-size:18px;border-bottom:1px solid #E7E7E7;}
+.content{padding:20px;}
 </style>
 </head>
 <body>
 <div id="main">
-    <div class="head"></div>
+    <div class="head">
+	    <div class="htit">成都启点科技系统操作知识库</div>
+		<div class="search">
+		     <form id="sform" method="post" action="/Operation">
+				<input type="text" class="keyword" name="keyword"  placeholder="请输入搜索内容"/>
+				<button type="button" class="btn btn-info searchbtn">搜索</button>
+			</form>
+		</div>
+	</div>
 	<div>
 	    <div class="left">
 		    <div class="stit">知识分类</div>
-		    <div class="one">
-			    <div class="ptit"><span class="icon">+</span>软电话问题</div>
+			<?php if(is_array($list)): $i = 0; $__LIST__ = $list;if( count($__LIST__)==0 ) : echo "" ;else: foreach($__LIST__ as $key=>$vo): $mod = ($i % 2 );++$i;?><div class="one">
+			    <div class="ptit"><span class="icon">+</span><?php echo ($vo["name"]); ?></div>
 				<div class="otit">
-					<div class="ctit">222</div>
-					<div class="ctit">222</div>
-					<div class="ctit">222</div>
+				<?php if(is_array($vo["news"])): $i = 0; $__LIST__ = $vo["news"];if( count($__LIST__)==0 ) : echo "" ;else: foreach($__LIST__ as $key=>$vo1): $mod = ($i % 2 );++$i;?><div class="ctit" value="<?php echo ($vo1["id"]); ?>"><?php echo ($vo1["title"]); ?></div><?php endforeach; endif; else: echo "" ;endif; ?>
 				</div>
-			</div>
-			<div class="one">
-			    <div class="ptit"><span class="icon">+</span>软电话问题</div>
-				<div class="otit">
-					<div class="ctit">222</div>
-					<div class="ctit">222</div>
-					<div class="ctit">222</div>
-				</div>
-			</div>
-			<div class="one">
-			    <div class="ptit"><span class="icon">+</span>软电话问题</div>
-				<div class="otit">
-					<div class="ctit">222</div>
-					<div class="ctit">222</div>
-					<div class="ctit">222</div>
-					<div class="ctit">222</div>
-					<div class="ctit">222</div>
-					<div class="ctit">222</div>
-					<div class="ctit">222</div>
-					<div class="ctit">222</div>
-					<div class="ctit">222</div>
-					<div class="ctit">222</div>
-					<div class="ctit">222</div>
-					<div class="ctit">222</div>
-				</div>
-			</div>
-			
+			</div><?php endforeach; endif; else: echo "" ;endif; ?>
 		</div>
+		
 		<div class="right">
-		    <div class="dtit">软电话问题 > 打不出电话</div>
+		    <div class="dtit">&nbsp;</div>
+			<div class="content">
+			</div>
 		</div>
 	</div>
 	
@@ -70,9 +60,58 @@ body{background:#f5f5f5;}
 <script>
 $('.ptit').click(function(){
    $(this).siblings('.otit').slideToggle('100');
+   $(this).parent().siblings('.one').children('.otit').slideUp('100');
    var html = $(this).children('.icon').html()=='+'? '-':'+';
    $(this).children('.icon').html(html);
+   $(this).parent().siblings('.one').find('.icon').html('+');
   
 });
 
+$('.ctit').click(function(){
+    var id=$(this).attr('value');
+	contentAjax(id);
+});
+
+$('.searchbtn').click(function(){
+   var show = false;
+   var keyword = $('.keyword').val();
+   if(keyword ==''){
+     $('.keyword').focus();
+   }else{
+      $('.ctit').each(function(){
+	    // alert($(this).html().indexOf(keyword));
+	     if($(this).html().indexOf(keyword)>=0){
+		     $(this).parent().show();
+			 $(this).parent().siblings('.ptit').find(' .icon').html('-');
+			 $(this).css('color','red');
+			 if(show == false){
+			    var id = $(this).attr('value');
+				contentAjax(id);
+				show = true;
+			 }
+		 }else{
+			 $(this).css('color','#666');
+		 }
+		
+	  });
+     // $('#sform').submit();
+   }
+});
+
+function contentAjax(id){
+$.ajax({
+	  url: "/Operation/getContent",
+	  method: "POST",
+	  data: {id : id},
+	  success: function(data){
+	      if(data.status == 1){
+		    $('.content').html(data['msg']['content']);
+			$('.dtit').html(data['msg']['pname']+ " > " + data['msg']['title']);			
+		  }else{
+		    $('.content').html(data['msg']);
+		  }
+	      
+	  }
+	});
+}
 </script>
